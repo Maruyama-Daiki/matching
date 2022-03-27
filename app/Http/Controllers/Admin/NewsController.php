2792186254397747555eb57654f0f6d26c47dba5
daiki maruyama <maruden0103@gmail.com>
2022-03-27 08:39:00 +0000

    message

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -34,7 +34,7 @@ class NewsController extends Controller
       // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
       if (isset($form['image_path'])) {
         $path = $request->file('image_path')->store('public/image');
-        $news->image_path = basename($path);
+        $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news->image_path = null;
       }
